ba4592228a25a4d617ea19b14f6f3326c6cfcecc
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-18 16:40:30 +0200

    feat: html elements inits

---
+++ js/modules/elements.mjs
@@ -0,0 +1,7 @@
+const outputElement = document.getElementById('result');
+const textInputElement = document.getElementById('text');
+const stepsInputElement = document.getElementById('steps');
+const decryptBtn = document.getElementById('decryptBtn');
+const encryptBtn = document.getElementById('encryptBtn');
+
+export {outputElement, textInputElement, stepsInputElement, decryptBtn, encryptBtn}
